88a8d33a993345806337d7fb47a70f660366edab
오세천 <zidanpark@hanmall.net>
2018-07-14 21:04:52 +0900

    strategy test

---
+++ patten/strategy/index.php
@@ -0,0 +1,16 @@
+<?
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+include "./testClass.php";
+
+
+$duckType = "duck";
+$test = new tesCtlass($duckType);
+$test->flyingBehavior(new simpleFlying());
+$duck = $test->productDuck();
+echo $duck->type;
+echo "<br>";
+echo $duck->fly;
+
+
