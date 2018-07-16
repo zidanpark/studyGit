70e204db2d25d384c9caf2c6bc3943d9e0a6342a
오세천 <zidanpark@gmail.com>
2018-07-16 20:25:53 +0900

    test

--- patten/strategy/index.php
+++ patten/strategy/index.php
@@ -1,7 +1,5 @@
 <?
-<<<<<<< HEAD
-    echo "Å×½ºÆ®2";
-=======
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
@@ -16,5 +14,3 @@ echo $duck->type;
 echo "<br>";
 echo $duck->fly;
 
-
->>>>>>> 88a8d33a993345806337d7fb47a70f660366edab
