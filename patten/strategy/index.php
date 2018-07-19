db6908571c7b91c582743496a569a77f655c8622
[개발]오세천 <zidanpark@hanmail.net>
2018-07-19 11:37:08 +0900

    commit

--- patten/strategy/index.php
+++ patten/strategy/index.php
@@ -1,7 +1,4 @@
 <?
-<<<<<<< HEAD
-    echo "Å×½ºÆ®2";
-=======
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
@@ -10,11 +7,9 @@ include "./testClass.php";
 
 $duckType = "duck";
 $test = new tesCtlass($duckType);
-$test->flyingBehavior(new simpleFlying());
+$test->flyingBehavior(new noFlying());
 $duck = $test->productDuck();
+
 echo $duck->type;
 echo "<br>";
 echo $duck->fly;
-
-
->>>>>>> 88a8d33a993345806337d7fb47a70f660366edab
