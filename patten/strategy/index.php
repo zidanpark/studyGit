d8f59b21e5b9cc540539a1c70c3a0ccdba31c72c
[개발]오세천 <zidanpark@hanmail.net>
2018-07-23 09:10:04 +0900

    closer

--- patten/strategy/index.php
+++ patten/strategy/index.php
@@ -1,8 +1,4 @@
 <?
-<<<<<<< HEAD
-=======
-
->>>>>>> 70e204db2d25d384c9caf2c6bc3943d9e0a6342a
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
@@ -16,9 +12,4 @@ $duck = $test->productDuck();
 
 echo $duck->type;
 echo "<br>";
-<<<<<<< HEAD
 echo $duck->fly;
-=======
-echo $duck->fly;
-
->>>>>>> 70e204db2d25d384c9caf2c6bc3943d9e0a6342a
